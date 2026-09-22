9133c2170bdf9844cb0fdd15e71570d5c0674e2c
Muhammad <muhammadnazirqulov1@gmail.com>
2026-09-22 16:07:40 +0500

    first

--- src/main/java/com/example/springBoot2/service/ProductService.java
+++ src/main/java/com/example/springBoot2/service/ProductService.java
@@ -11,7 +11,7 @@ import java.util.List;
 @Service
 @RequiredArgsConstructor
 public class ProductService {
-
+//ali
     private final ProductRepository productRepository;
 
     public String saveProduct(ProductDTO productDTO) {
